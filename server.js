3e4670e6867acdbeedfea6240b76c1abd2a2b138
Jason Wong <jason_wong_9@alumni.ubc.ca>
2016-01-17 05:24:07 +0000

    Added Partials and Index EJS

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.set('views', __dirname + '/server/views');
 
 app.set('view engine', 'ejs');
 
+app.get("/", function(req, res){
+   res.render("index");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started!!");
 });
