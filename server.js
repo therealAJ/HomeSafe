b92f4309e2c30822525a2d2757ba99466edd139c
Alex <alexonej@gmail.com>
2016-03-27 13:07:08 -0700

    refactor workspace

--- server.js
+++ server.js
@@ -1,20 +1,2 @@
 var express = require("express");
 var app = express();
-
-app.set('views', __dirname + '/server/views');
-
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/partials/:partialPath', function(req, res) {
-    res.render('partials/' + req.params.partialPath);
-})
-
-app.get("/", function(req, res){
-   res.render("index");
-});
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("server started!!");
-});
