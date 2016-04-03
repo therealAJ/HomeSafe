03f9243b19b6a26ee98f89d0ac29c2bb4d1f5a86
Alex <alexonej@gmail.com>
2016-04-03 02:28:22 -0700

    connect database

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var morgan = require('morgan')
+var mongoose = require('mongoose')
+var config = require('./config.js');
 
 var app = express();
+
+
+mongoose.connect(config.database, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to database");
+    }
+})
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,7 +28,7 @@ app.get('/', function (req, res) {
     res.sendfile('./public/app/views/index.html');
 })
 
-app.listen(8080, function (err) {
+app.listen(config.port, function (err) {
     if (err) {
         console.log(err);
     } else {
