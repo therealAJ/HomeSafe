11ee9fc9b54914c4bfcaf9ed7ff334fb42530112
Alex <alexonej@gmail.com>
2016-04-12 15:23:17 -0700

    login api

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,12 +1,28 @@
 var User = require('../models/user.js');
 var config = require('../../config.js');
 var secretKey = config.secretKey;
+var jsonwebtoken = require('jsonwebtoken');
 
+function createToken(user) {
+    
+   var token = jsonwebtoken.sign({
+            _id: user._id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            username: user.username,
+        },  secretKey, {
+            expiresInMinute: 1440 
+        });
+    
+    return token; 
+}
 
 module.exports = function (app, express) {
 
 
     var api = express.Router();
+    
+    //SIGNUP API
 
     api.post('/signup', function (req, res) {
 
@@ -33,6 +49,8 @@ module.exports = function (app, express) {
         });
     });
     
+    // GET ALL USERS API
+    
     api.get('/users', function(req, res) {
      
             User.find({}, function(err, users){
@@ -41,9 +59,42 @@ module.exports = function (app, express) {
                     return; 
                 }
                 res.json(users); 
-            })
+            });
+    });
+    
+    // LOGIN API 
+    
+    api.post('/login', function(req, res){
+        
+        User.findOne({
+            username: req.body.username 
             
-    })
+        }).select('password').exec(function (err, user){
+            
+            if(err) throw err;
+            
+            if(!user) {
+                res.send({ message: "User doesn't exist"});   
+            } else if(user) {
+                
+                var validPassword = user.comparePassword(req.body.password);
+                
+                if(!validPassword) {
+                    res.send({ message: "Invalid Password"});
+                } else {
+                    
+                    //token 
+                    var token = createToken(user); 
+                    
+                    res.json({
+                        success: true, 
+                        message: "Successful login!",
+                        token: token
+                    });    
+                }
+            }
+        });
+    });
     
     
     
