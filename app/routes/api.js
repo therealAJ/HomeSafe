1e3993f1a85872f5555ea7c2e981f0f7951f8728
Alex <alexonej@gmail.com>
2016-04-13 10:45:37 -0700

    starting to add middleware for token verification

--- app/routes/api.js
+++ app/routes/api.js
@@ -97,8 +97,33 @@ module.exports = function (app, express) {
     });
     
     
+    app.use(function(req,res, next){
+        
+        console.log("Somebody just came to our app!"); 
+        
+        var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+        
+        // check if token exists
+        if(token) {
+            
+            jsonwebtoken.verify(token, secretKey, function(err, decoded){
+                
+                if(err) {
+                    res.status(403).send({ success: false, message: "Failed to authenticate user"});
+                    
+                } else {
+                    
+                    req.decoded = decoded; 
+                 
+                    next(); 
+                }
+            });
+        } else {
+            res.status(403).send({ sucess: false, message: "No Token Provided"});
+        }
+    });
     
     
-
     return api; 
+    
 }
