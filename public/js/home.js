957dc40fb92a200237c0e4c7ea1b1a49e1f0adf2
Alex <alexonej@gmail.com>
2016-06-28 13:34:50 -0700

    added safewalk markers to fragment

--- public/js/home.js
+++ public/js/home.js
@@ -8,4 +8,33 @@
          },
          zoom: 15
      });
+
+
+     var locations = [
+      ['Safe Walk via UBC Village', 49.266316, -123.24361, 1],
+      ['Safe Walk via Walter Gage Residence', 49.27, -123.25, 2],
+      ['Safe Walk via Irving Library', 49.267581, -123.252321, 3],
+      ['Safe Walk via Safe On Foods', 49.254, -123.24, 4],
+      ['Safe Walk via Wreck Beach', 49.262, -123.2581, 5],
+      ['Safe Walk via Thunderbird Stadium', 49.2554, -123.246, 6],
+      ['Safe Walk via Museum of Anthropology', 49.2683, -123.259095, 7]
+    ];
+
+     var infowindow = new google.maps.InfoWindow();
+
+     var marker, i;
+
+     for (i = 0; i < locations.length; i++) {
+         marker = new google.maps.Marker({
+             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+             map: map
+         });
+
+         google.maps.event.addListener(marker, 'click', (function (marker, i) {
+             return function () {
+                 infowindow.setContent(locations[i][0]);
+                 infowindow.open(map, marker);
+             }
+         })(marker, i));
+     }
  }
