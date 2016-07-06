6698ebf2a702fe0405bc3b478a4e088eaa91e35e
Alex <alexonej@gmail.com>
2016-07-06 00:13:58 -0700

    added distance tracking to each marker.. have to figure out how to extract geolocation still

--- public/js/home.js
+++ public/js/home.js
@@ -9,13 +9,12 @@
          zoom: 15
      });
 
-
      // Get user location and place marker on map, open infowindow
 
      var currentPosition = new google.maps.Marker({
          map: map,
      });
-     var currentPositionInfoWindow = new google.maps.InfoWindow(); 
+     var currentPositionInfoWindow = new google.maps.InfoWindow();
 
      if (navigator.geolocation) {
          navigator.geolocation.getCurrentPosition(function (position) {
@@ -45,11 +44,12 @@
      }
 
 
+
      var locations = [
       ['Safe Walk via UBC Village', 49.266316, -123.24361, 1],
       ['Safe Walk via Walter Gage Residence', 49.27, -123.25, 2],
       ['Safe Walk via Irving Library', 49.267581, -123.252321, 3],
-      ['Safe Walk via Safe On Foods', 49.254, -123.24, 4],
+      ['Safe Walk via Save On Foods', 49.254, -123.24, 4],
       ['Safe Walk via Wreck Beach', 49.262, -123.2581, 5],
       ['Safe Walk via Thunderbird Stadium', 49.2554, -123.246, 6],
       ['Safe Walk via Museum of Anthropology', 49.2683, -123.259095, 7]
@@ -72,4 +72,49 @@
              }
          })(marker, i));
      }
+
+
+     var shortestPaths = []
+     var ionaLatLong = new google.maps.LatLng(49.2704778, -123.2525351);
+     
+     // TODO: Figure out how to get current position's LatLng into global var
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(function (position) {
+//             var pos = {
+//                 lat: position.coords.latitude,
+//                 lng: position.coords.longitude
+//             };
+//
+//             ionaLatLong = pos;
+//
+//         }, function () {
+//             handleLocationError(true, currentPosition, map.getCenter());
+//         });
+//     } else {
+//         // Browser doesn't support Geolocation
+//         handleLocationError(false, currentPosition, map.getCenter());
+//     }
+
+
+     for (i = 0; i < locations.length; i++) {
+         var latLngA = new google.maps.LatLng(locations[i][1], locations[i][2]);
+         var distance = Math.round((google.maps.geometry.spherical.computeDistanceBetween(latLngA, ionaLatLong) / 1000) * 100) / 100;
+
+         shortestPaths[i] = distance;
+     }
+
+     var rows = 7,
+         cols = 2;
+
+     for (var i = 0; i < rows; i++) {
+         $('table').append('<tr></tr>');
+         for (var j = 0; j < cols; j++) {
+             $('table').find('tr').eq(i).append('<td></td>');
+             $('table').find('tr').eq(i).find('td').eq(j).attr('data-row', i).attr('data-col', j);
+             $('table').find('tr').eq(i).find('td').eq(0).text(locations[i][0]);
+             $('table').find('tr').eq(i).find('td').eq(1).text(shortestPaths[i] + " km");
+             //locations[i][1]
+         }
+     }
+
  }
