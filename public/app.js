74c27f4045cecf4f984d2c654124a6c437170c37
David Baldwynn <whitef0x0@users.noreply.github.com>
2016-07-21 01:34:14 -0700

    Delete app.js

--- public/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp', [
-    'ngRoute']).
-    config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-        $routeProvider.when('/home', {templateUrl: "/partials/home", controller: "homeController"});
-        $routeProvider.otherwise('/home');
-
-        $locationProvider.html5Mode({enabled: true, requireBase: false});
-    }]);
