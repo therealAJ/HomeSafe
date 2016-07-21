5db4af21cf855b157e438f6d02309d840fc3bb5e
David Baldwynn <whitef0x0@users.noreply.github.com>
2016-07-21 10:31:56 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -222,6 +222,7 @@
 
 function computeTotalDistance(result) {
   var total = 0;
+  console.log(result)
   var myroute = result.routes[0];
   for (var i = 0; i < myroute.legs.length; i++) {
     total += myroute.legs[i].distance.value;
